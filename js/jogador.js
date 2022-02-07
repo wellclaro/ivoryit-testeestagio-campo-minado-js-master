5255c387350129c690634e587ee41ce89aca2f69
welly <wellyngton.claro@gmmail.com>
2022-02-07 17:34:10 -0300

    Star solution

---
+++ js/jogador.js
@@ -0,0 +1,22 @@
+
+
+function Jogador(jogo){
+    var escolhas=[];
+
+    function LerJogo(tabuleiro) {
+       for(var i=0;i<tabuleiro.length;i++){
+            for(var j=0; j<tabuleiro[i].length;j++){
+                if (tabuleiro[i][j]=='-'){
+
+                }
+            }
+        }
+    }
+
+    function Jogada(escolhas){
+        for (var i=0;i<escolhas.length;i++){
+            jogo.Abrir();
+        }
+    }
+
+}
